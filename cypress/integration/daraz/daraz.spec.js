28bf0fbfade45cffae9cd9a1f89ca055cc1f009a
snmmhrj <snmmhrj@gmail.com>
2021-04-16 14:15:23 +0545

    added docker file

--- cypress/integration/daraz/daraz.spec.js
+++ cypress/integration/daraz/daraz.spec.js
@@ -1,7 +1,20 @@
-describe('Sign up', function () {
-    it('Visit the daraz site', function () {
-        cy.visit('https://www.daraz.com.np')
+describe('Daraz', function () {
+    it('Visit the daraz site and go to sign up page', function () {
+        cy.visit('https://www.google.com')
         cy.title().should('eq', 'Online Shopping in Nepal | Best Deals, Prices & Discounts - Daraz.com.np')
         cy.get('#anonSignup').contains('sign up').click()
+        
+
+        })
+        it('sign up', function () {
+            cy.get('form').within(($form) => {
+               // cy.root().submit()  
+             //cy.get('button[type = "button"]').click() 
+             cy.get('.mod-change-register-btn').click()
+             //cy.get('.mod-input mod-login-input-email mod-input-email', { timeout: 10000}).click().type('daraz@mailinator.com')
+             cy.xpath('')
+
+        })
+    
     }) 
     })
---
+++ dockerfile
@@ -0,0 +1,1 @@
+FROM cypress/included:7.1.0
