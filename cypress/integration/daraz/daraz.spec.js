65cb01c92f923a4f5ba527f56f507bb5ae3860b0
snmmhrj <snmmhrj@gmail.com>
2021-04-21 09:22:11 +0545

    free delivery page, and verify link of app

--- cypress/integration/daraz/daraz.spec.js
+++ cypress/integration/daraz/daraz.spec.js
@@ -82,25 +82,56 @@ it('Search Oliz Store and go to Oliz Home page', function () {
     cy.xpath("//input[@type='search' and @placeholder='Search in Daraz']").type("Oliz Store")
     cy.get('.search-box__search--2fC5').click()
     cy.xpath("//div[@class='cRjKsc']//img", { timeout: 10000 }).eq(0).should('be.visible').click({ force: true })
+    cy.wait(3000)
     cy.xpath("//div[@class='seller-name__wrapper']//a[@href]").scrollIntoView().should('be.visible').click({ force: true })
     cy.url().should("include", "/shop/oliz-store")
 })
 
 it('Enter free delivery page and click shop now', function () {
-    cy.get('[href="//www.daraz.com.np/wow/gcp/daraz/channel/np/LP/free-delivery-week-electronics?scm=1003.4.icms-zebra-100031662-2974971.OTHER_6043412089_7337945"] > .lzd-menu-labels-item-text').click()
-    cy.get('[data-aplus-ae="x2_60a50fc3"] > .product-item-bottom > [style="border: 0px solid black; box-sizing: border-box; display: flex; -webkit-box-orient: horizontal; flex-direction: row; place-content: flex-start center; flex-shrink: 0; cursor: pointer; position: relative; border-radius: 4px; background-image: linear-gradient(90deg, rgb(251, 148, 68) 5%, rgb(245, 114, 36) 100%); text-align: center; -webkit-box-align: center; align-items: center; -webkit-box-pack: center; padding: 12px; width: auto; height: 28px;"] > .text').click()
+    cy.wait(2000)
+    cy.get('[href="//www.daraz.com.np/wow/gcp/daraz/channel/np/LP/free-delivery-week-electronics?scm=1003.4.icms-zebra-100031662-2974971.OTHER_6043462288_7341344"] > .lzd-menu-labels-item-text').click()
+    // cy.get('[data-aplus-ae="x2_60a50fc3"] > .product-item-bottom > [style="border: 0px solid black; box-sizing: border-box; display: flex; -webkit-box-orient: horizontal; flex-direction: row; place-content: flex-start center; flex-shrink: 0; cursor: pointer; position: relative; border-radius: 4px; background-image: linear-gradient(90deg, rgb(251, 148, 68) 5%, rgb(245, 114, 36) 100%); text-align: center; -webkit-box-align: center; align-items: center; -webkit-box-pack: center; padding: 12px; width: auto; height: 28px;"] > .text').click()
+    // cy.xpath("//*[@id="5213589410"]/div/div/div[1]/a[1]/div/div[2]").click()
+    cy.xpath("//div[@class='cRjKsc']//img", { timeout: 10000 }).eq(0).should('be.visible').click({ force: true })
     cy.get('.delivery-option-item__shipping-fee').should('have.attr','Free')
 })
 
+// it('Ask Question and verify question', function () {
+//     cy.xpath("//div//button[text()='ASK QUESTIONS']", { timeout: 10000 }).scrollIntoView().should('be.visible');
+
+//         // cy.intercept("POST", "/pdp/item/addItemSkuQA").as('askQuestion');
+
+//         cy.intercept("POST", "/pdp/item/addItemSkuQA").as("askQuestion");
+
+//         // cy.intercept('POST', '/pdp/item/addItemSkuQA', {
+//         //     // fixture: 'askQuestion.json'
+//         // }).as('addFacility')
+
+//         // cy.xpath("//div[contains(@class,'qna-ask-box')]").eq(0).scrollIntoView().should('be.visible').click().type('Random Question Posted by Pranesh');
+
+//         cy.xpath("//div[contains(@class,'qna-ask-box')]").eq(0).scrollIntoView().should('be.visible').click().type('Any New Year Offer on price discount?');
+
+//         cy.xpath("//div//button[text()='ASK QUESTIONS']").click();
+
+
+//         // cy.intercept("POST", "https://my.daraz.com.np/pdp/item/addItemSkuQA", { content: 'ABC Pranesh', statusCode: 200 }).as("askQuestion");
 
 
 
+
+//         cy.wait('@askQuestion').then((resp) => {
+//             cy.log(resp);
+//             console.log(resp);
   
-    
+// })
 
-
-
-
+it('Click on Save more on app and verify link', function () {
+    cy.get('#topActionDownload').click()
+    cy.xpath("//div[@class='app-stores']//a").eq(0).should('have.attr', 'href').and('include', 'itunes.apple.com/app/id978058048')
+    cy.xpath("//div[@class='app-stores']//a").eq(1).should('have.attr', 'href').and('include', 'play.google.com/store/apps/details?id=com.daraz.android')
 
 })
+
+})
+
       
